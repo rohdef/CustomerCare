683f46fd1679a8b3ffe42ce4a126b5dfb8c0a8aa
Rohde Fischer <krdata@gmail.com>
2010-11-09 15:10:56 +0100

    Initial import

---
+++ src/as/markon/client/CustomerCare.java
@@ -0,0 +1,11 @@
+package as.markon.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class CustomerCare implements EntryPoint {
+	/**
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad() {
+	}
+}
