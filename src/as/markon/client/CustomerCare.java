6a49eabc18c5f16326b32d5163b5ed7693f44699
Rohde Fischer <krdata@gmail.com>
2010-11-10 09:13:46 +0100

    Markon det John

--- src/as/markon/client/CustomerCare.java
+++ src/as/markon/client/CustomerCare.java
@@ -1,11 +1,27 @@
 package as.markon.client;
 
+import com.extjs.gxt.ui.client.event.ButtonEvent;
+import com.extjs.gxt.ui.client.event.SelectionListener;
+import com.extjs.gxt.ui.client.widget.MessageBox;
+import com.extjs.gxt.ui.client.widget.button.Button;
+import com.extjs.gxt.ui.client.widget.button.ButtonBar;
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.RootPanel;
 
 public class CustomerCare implements EntryPoint {
 	/**
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
+		ButtonBar b = new ButtonBar();
+		
+		b.add(new Button("Telefonen ringer", new SelectionListener<ButtonEvent>() {
+			@Override
+			public void componentSelected(ButtonEvent ce) {
+				MessageBox.confirm("Svaret er", "Markon det John", null);
+			}
+		}));
+		
+		RootPanel.get().add(b);
 	}
 }
---
+++ src/as/markon/server/GreetingServiceImpl.java
@@ -0,0 +1,11 @@
+package as.markon.server;
+
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+/**
+ * The server side implementation of the RPC service.
+ */
+@SuppressWarnings("serial")
+public class GreetingServiceImpl extends RemoteServiceServlet {
+
+}
