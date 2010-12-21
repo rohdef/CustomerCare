fd736d27af4d4d14decc16811bd4b8c6689dd138
Rohde Fischer <krdata@gmail.com>
2010-12-21 10:20:19 +0100

    Bug in pdf generation fixed

--- src/as/markon/server/PdfServlet.java
+++ src/as/markon/server/PdfServlet.java
@@ -31,15 +31,18 @@ import com.itextpdf.text.pdf.PdfWriter;
 
 public class PdfServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private Logger logger = Logger.getLogger(DatabaseServiceImpl.class);
+	private Logger logger = Logger.getLogger(PdfServlet.class);
 	
 	private PdfPTable createTable(List<LabelData> companies) {
+		logger.debug("Creating PDF table for " + companies.size() + " companies");
+		
 		PdfPTable table = new PdfPTable(2);
 		table.setWidthPercentage(100f);
 		float cellHeight = PageSize.A4.getHeight()/4;
 		table.getDefaultCell().setHorizontalAlignment(Element.ALIGN_CENTER);
 		
 		for (LabelData c : companies) {
+			logger.debug("\tAdding the company " + c.company + " to the PDF.");
 			PdfPCell cell = new PdfPCell();
 			cell.setBorder(0);
 			cell.setPadding(5f);
@@ -57,10 +60,20 @@ public class PdfServlet extends HttpServlet {
 				cell.addElement(phrase);
 			}
 			
+			logger.debug("\tCreated the phrase for the company.");
+			
 			cell.setFixedHeight(cellHeight);
 			table.addCell(cell);
+			
+			logger.debug("\tAdded the company " + c.company);
 		}
 		
+		if (companies.size() % 2 != 0) {
+			PdfPCell cell = new PdfPCell();
+			cell.setBorder(0);
+			table.addCell(cell);
+		}
+
 		return table;
 	}
 	
@@ -77,6 +90,8 @@ public class PdfServlet extends HttpServlet {
 		if (id == null)
 			throw new NullPointerException("The id is set wrong");
 		
+		connect();
+		
 		try {
 			OutputStream str = resp.getOutputStream();
 			List<LabelData> companies = getCompaniesFor(Integer.parseInt(id.trim()));
@@ -86,6 +101,9 @@ public class PdfServlet extends HttpServlet {
 		
 			doc.open();
 			doc.add(createTable(companies));
+			doc.addCreator("Rohde Fischer's CustomerCare");
+			doc.addCreationDate();
+			doc.addTitle("Labels til print");
 			doc.close();
 			str.flush();
 		} catch (DocumentException e) {
@@ -107,6 +125,73 @@ public class PdfServlet extends HttpServlet {
 	private List<LabelData> getCompaniesFor(int batchId) {
 		List<LabelData> labels = new ArrayList<LabelData>();
 		
+		try {
+			Statement companyStatement;
+			companyStatement = c.createStatement();
+
+			String companyQuery = "SELECT c.companyname, c.address, c.postal, p.city, \n" +
+					"\tk.contactname\n" +
+					"\tFROM labelqueue q, contacts k, companies c, postalcodes p\n" +
+					"\tWHERE q.companyid = c.companyid AND q.contactid = k.contactid " +
+					"\tAND c.postal=p.postal AND q.batchid="+batchId+";";
+			
+			logger.debug("Running the following query:\n" + companyQuery);
+			
+			ResultSet companyResults;
+			companyResults = companyStatement.executeQuery(companyQuery);
+
+			while (companyResults.next()) {
+				LabelData ld = new LabelData();
+				ld.company = companyResults.getString("companyname");
+				ld.address = companyResults.getString("address");
+				ld.city = companyResults.getInt("postal") + " "
+					+ companyResults.getString("city");
+				ld.attention = companyResults.getString("contactname");
+				
+				logger.debug("Adding the label for: " + ld.company +
+						" and the att.: " + ld.attention);
+				
+				labels.add(ld);
+			}
+		} catch (Exception e) {
+			logger.fatal("Trying to insert company", e);
+			throw new RuntimeException("Kunne ikke hente labels med kunde.");
+		}
+		
+		try {
+			Statement companyStatement;
+			companyStatement = c.createStatement();
+
+			String companyQuery = "SELECT c.companyname, c.address, c.postal, p.city\n" +
+					"\tFROM labelqueue q, companies c, postalcodes p\n" +
+					"\tWHERE q.companyid = c.companyid AND q.contactid IS NULL " +
+					"\tAND c.postal=p.postal AND batchid="+batchId+";";
+			logger.debug("Running the following query:\n" + companyQuery);
+			
+			ResultSet companyResults;
+			companyResults = companyStatement.executeQuery(companyQuery);
+
+			while (companyResults.next()) {
+				LabelData ld = new LabelData();
+				ld.company = companyResults.getString("companyname");
+				ld.address = companyResults.getString("address");
+				ld.city = companyResults.getInt("postal") + " "
+					+ companyResults.getString("city");
+				ld.attention = null;
+				
+				logger.debug("Adding the label for: " + ld.company);
+				
+				labels.add(ld);
+			}
+		} catch (Exception e) {
+			logger.fatal("Getting queue", e);
+			throw new RuntimeException("Kunne ikke hente labels uden kunde");
+		}
+		
+		return labels;
+	}
+
+	private void connect() {
 		try {
 			XMLConfiguration config = new XMLConfiguration("config.xml");
 			
@@ -137,61 +222,6 @@ public class PdfServlet extends HttpServlet {
 			logger.fatal("Database connection failed", e);
 			throw new RuntimeException("Kunne ikke oprette forbindelse til databasen.");
 		}
-		
-		try {
-			Statement companyStatement;
-			companyStatement = c.createStatement();
-
-			String companyQuery = "SELECT c.companyname, c.address, c.postal, p.city, \n" +
-					"\tk.contactname\n" +
-					"\tFROM labelqueue q, contacts k, companies c, postalcodes p\n" +
-					"\tWHERE q.companyid = c.companyid AND q.contactid = k.contactid " +
-					"\tAND c.postal=p.postal AND q.batchid="+batchId+";";
-			
-			ResultSet companyResults;
-			companyResults = companyStatement.executeQuery(companyQuery);
-
-			while (companyResults.next()) {
-				LabelData ld = new LabelData();
-				ld.company = companyResults.getString("companyname");
-				ld.address = companyResults.getString("address");
-				ld.city = companyResults.getInt("postal") + " "
-					+ companyResults.getString("city");
-				ld.attention = companyResults.getString("contactname");
-				labels.add(ld);
-			}
-		} catch (Exception e) {
-			logger.fatal("Trying to insert company", e);
-			throw new RuntimeException("Kunne ikke hente labels med kunde.");
-		}
-		
-		try {
-			Statement companyStatement;
-			companyStatement = c.createStatement();
-
-			String companyQuery = "SELECT c.companyname, c.address, c.postal, p.city\n" +
-					"\tFROM labelqueue q, companies c, postalcodes p\n" +
-					"\tWHERE q.companyid = c.companyid AND q.contactid IS NULL " +
-					"\tAND c.postal=p.postal AND batchid="+batchId+";";
-			
-			ResultSet companyResults;
-			companyResults = companyStatement.executeQuery(companyQuery);
-
-			while (companyResults.next()) {
-				LabelData ld = new LabelData();
-				ld.company = companyResults.getString("companyname");
-				ld.address = companyResults.getString("address");
-				ld.city = companyResults.getInt("postal") + " "
-					+ companyResults.getString("city");
-				ld.attention = null;
-				labels.add(ld);
-			}
-		} catch (Exception e) {
-			logger.fatal("Getting queue", e);
-			throw new RuntimeException("Kunne ikke hente labels uden kunde");
-		}
-		
-		return labels;
 	}
 	
 	private class LabelData {
