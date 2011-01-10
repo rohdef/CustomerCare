ab061d2af35861970847688e17cd929cca43f6c3
Rohde Fischer <krdata@gmail.com>
2011-01-10 13:49:04 +0100

    Started internatiolizing (although one string is rather pathetic :p )

--- src/dk/rohdef/client/CustomerCare.java
+++ src/dk/rohdef/client/CustomerCare.java
@@ -4,9 +4,15 @@ import com.extjs.gxt.ui.client.event.Events;
 import com.extjs.gxt.ui.client.event.Listener;
 import com.extjs.gxt.ui.client.event.SelectionEvent;
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 
+import dk.rohdef.client.i18n.CustomerCareI18n;
+import dk.rohdef.client.services.DataServiceAsync;
+import dk.rohdef.viewmodel.Company;
 import dk.rohdef.viewmodel.Salesman;
 
 public class CustomerCare implements EntryPoint {
@@ -15,12 +21,15 @@ public class CustomerCare implements EntryPoint {
 	 */
 	public void onModuleLoad() {
 		final Login l = new Login();
+		final DataServiceAsync dataservice =
+			dk.rohdef.client.services.Global.getInstance().getDataService();
+		final CustomerCareI18n i18n = GWT.create(CustomerCareI18n.class);
 		
 		l.addListener(Events.Select, new Listener<SelectionEvent<Salesman>>() {
 			public void handleEvent(SelectionEvent<Salesman> be) {
 				RootPanel.get().remove(l);
 				RootPanel.get().add(new CustomerView(be.getModel()));
-				dk.rohdef.client.services.Global.getInstance().getDataService().loaded(
+				dataservice.loaded(
 						new AsyncCallback<Void>() {
 							public void onSuccess(Void result) {
 							}
@@ -30,6 +39,16 @@ public class CustomerCare implements EntryPoint {
 						});
 			}
 		});
+		
+		dataservice.getAppCompany(new AsyncCallback<Company>() {
+			public void onSuccess(Company result) {
+				Window.setTitle(i18n.pageTitle(result.getCompanyName()));
+			}
+			
+			public void onFailure(Throwable caught) {
+			}
+		});
+		
 		RootPanel.get().add(l);
 	}
 }
